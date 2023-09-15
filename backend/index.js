e802fdc3c0c337d6d1d5f06d7625701b3d1dc1dd
MOHDSAMIULLAH <91786605+MOHDSAMIULLAH@users.noreply.github.com>
2023-09-15 06:24:07 +0000

    routes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const app = express();
+const dotenv = require("dotenv");
 const port = process.env.PORT || 5000;
 
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const courseRoute = require("./routes/course");
+const CourseCategoryRoute = require("./routes/course-category");
+const collegeRoute = require("./routes/college");
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+dotenv.config();
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://zeenatfirdoshquadri:zeenatfirdoshquadri@cluster0.2n16dzo.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -23,6 +28,11 @@ db.once('open', () => {
 // Define Mongoose Schema and Models for educational content (e.g., courses, articles, videos)
 
 // Define your routes for creating, updating, and retrieving educational content
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/course", courseRoute);
+app.use("/api/course-category", CourseCategoryRoute);
+app.use("/api/college", collegeRoute);
 
 // Start the server
 app.listen(port, () => {
